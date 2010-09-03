8d5817fedc0b6ab3cff45723186814c77d860964
Matthew Morgan <lytithwyn@gmail.com>
2010-09-03 15:45:15 -0400

    Commented out acb_fields.  This is defined in the header, not just declared.  Because of this, we're getting multiple definition errors on link.  We'll probably need to pull this out into an extern eventually.

--- include/sam.h
+++ include/sam.h
@@ -108,24 +108,24 @@ struct user_F {
 /* Seems not to be used on failed console logins at least */
 #define ACB_AUTOLOCK   0x0400  /* Account auto locked */
 
-char *acb_fields[16] = { /* explicit casts to char* added by Matthew Morgan */
-   (char *)"Disabled" ,
-   (char *)"Homedir req." ,
-   (char *)"Passwd not req." ,
-   (char *)"Temp. duplicate" ,
-   (char *)"Normal account" ,
-   (char *)"NMS account" ,
-   (char *)"Domain trust act." ,
-   (char *)"Wks trust act." ,
-   (char *)"Srv trust act" ,
-   (char *)"Pwd don't expire" ,
-   (char *)"Auto lockout" ,
-   (char *)"(unknown 0x08)" ,
-   (char *)"(unknown 0x10)" ,
-   (char *)"(unknown 0x20)" ,
-   (char *)"(unknown 0x40)" ,
-   (char *)"(unknown 0x80)" ,
-};
+//char *acb_fields[16] = { /* explicit casts to char* added by Matthew Morgan */
+//   (char *)"Disabled" ,
+//   (char *)"Homedir req." ,
+//   (char *)"Passwd not req." ,
+//   (char *)"Temp. duplicate" ,
+//   (char *)"Normal account" ,
+//   (char *)"NMS account" ,
+//   (char *)"Domain trust act." ,
+//   (char *)"Wks trust act." ,
+//   (char *)"Srv trust act" ,
+//   (char *)"Pwd don't expire" ,
+//   (char *)"Auto lockout" ,
+//   (char *)"(unknown 0x08)" ,
+//   (char *)"(unknown 0x10)" ,
+//   (char *)"(unknown 0x20)" ,
+//   (char *)"(unknown 0x40)" ,
+//   (char *)"(unknown 0x80)" ,
+//};
 
 /* Users V data struct */
 /* First 0xCC bytes is pointer & len table, rest is data which
