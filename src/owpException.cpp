aa853caec021ede5b31b17f47a23b18bff8b06a8
Matthew Morgan <lytithwyn@gmail.com>
2010-09-13 17:19:39 -0400

    Fixing the license text in all the "library" type files to be LGPL 2.1.

--- src/owpException.cpp
+++ src/owpException.cpp
@@ -5,18 +5,17 @@
  * Some code was borrowed/modified from the chntpw project
  * Copyright (c) 1997-2007 Petter Nordahl-Hagen
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2 of the License.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * See file LGPL.txt for the full license.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #include "include/owpException.h"
