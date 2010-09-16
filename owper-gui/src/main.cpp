f78fc34c911329209a7d32aa76505728ff0597d7
Matthew Morgan <lytithwyn@gmail.com>
2010-09-16 12:10:57 -0400

    Main now creates the gui and calls gtk_main().  We're graphical!

--- owper-gui/src/main.cpp
+++ owper-gui/src/main.cpp
@@ -26,6 +26,7 @@
 #include <dirent.h>
 #include <vector>
 #include <algorithm>
+#include <gtk/gtk.h>
 
 #include "include/ntreg.h"
 #include "include/sam.h"
@@ -34,6 +35,7 @@
 #include "include/fileManip.h"
 #include "include/owpException.h"
 #include "include/samHive.h"
+#include "include/owperGUI.h"
 
 using std::cout;
 using std::endl;
@@ -43,84 +45,17 @@ using stringManip::stringPrintf;
 using namespace owper;
 
 int main(int argc, char* argv[]) {
+    gtk_init(&argc, &argv);
+
     string testHive = "/media/disk/WINDOWS/system32/config/SAM";
     if(argc == 2) {
         testHive = argv[1];
     }
 
-    try {
-        owper::samHive *myHive = new samHive(testHive.c_str());
+    owperGUI *passwordClearer;
+    passwordClearer = new owperGUI();
 
-        cout << "Users:\n";
-        cout << "----------------------------------------\n";
-        cout << "User                          Password\n";
-        cout << "----------------------------------------\n";
-
-        int testUserIndex1 = -1;
-        int testUserIndex2 = -1;
-
-        for(unsigned int i = 0; i < myHive->getUserList().size(); i++) {
-            samUser *currentUser = myHive->getUserList().at(i);
-
-            cout << stringPrintf("Found user: %-17s %s",
-                    currentUser->getUserName().c_str(),
-                    ((currentUser->passwordIsBlank())?("Blank"):("Set"))) << endl;
-
-            if(currentUser->getUserName() == "Tracy") {
-                testUserIndex1 = i;
-            }
-
-            if(currentUser->getUserName() == "Bob") {
-                testUserIndex2 = i;
-            }
-        }
-
-        cout << endl;
-
-        cout << "Clearing Tracy's password" << endl;
-        myHive->getUserList().at(testUserIndex1)->blankPassword();
-
-        cout << "Merging all changes into hive in memory:" << endl;
-        if(myHive->mergeChangesToHive()) {
-            cout << "All changes successful!\n" << endl;
-        } else {
-            cout << "Some changes did not merge.\n" << endl;
-        }
-
-        cout << "Saving hive to file:" << endl;
-        if(myHive->writeHiveToFile()) {
-            cout << "Success!" << endl;
-        } else {
-            cout << "Failed!" << endl;
-        }
-
-        cout << "\nClearing Bob's password" << endl;
-        myHive->getUserList().at(testUserIndex2)->blankPassword();
-
-        cout << "Merging all changes into hive in memory:" << endl;
-        if(myHive->mergeChangesToHive()) {
-            cout << "All changes successful!\n" << endl;
-        } else {
-            cout << "Some changes did not merge.\n" << endl;
-        }
-
-        cout << "Saving hive to file:" << endl;
-        if(myHive->writeHiveToFile()) {
-            cout << "Success!" << endl;
-        } else {
-            cout << "Failed!" << endl;
-        }
-
-        for(unsigned int i = 0; i < myHive->getUserList().size(); i++) {
-            samUser *currentUser = myHive->getUserList().at(i);
-
-            cout << stringPrintf("Found user: %-17s %s",
-                    currentUser->getUserName().c_str(),
-                    ((currentUser->passwordIsBlank())?("Blank"):("Set"))) << endl;
-        }
-    }catch(exception& e) {
-        cout << e.what() << endl;
-    }
+    gtk_main();
 
     return 1;
 
